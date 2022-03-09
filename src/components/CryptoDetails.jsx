3ae2a27095fca92895f65e552b64d0a233c24ea7
Ayomide <ayomidedans@gmail.com>
2022-03-09 12:13:38 +0100

    init

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -11,17 +11,35 @@ const { Title, Text } = Typography;
 const { Option } = Select;
 
 const CryptoDetails = () => {
-  const { coinUuid } = useParams();
+  const { UUID } = useParams();
   // const [cryptos, setCryptos] = useState(data?.data?.coins.uuid);
   // const [timePeriod, setTimePeriod] = useState('7d');
-  const { data, isFetching } = useGetCryptoDetailsQuery(coinUuid);
+  const { data, isFetching } = useGetCryptoDetailsQuery(UUID);
 
-  console.log(coinUuid);
-  // console.log(data);
+  console.log(UUID);
+  console.log(data);
+
+  // const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+  // const stats = [
+  //   { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+  //   { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+  //   { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+  //   { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+  //   { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+  // ];
+
+  // const genericStats = [
+  //   { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+  //   { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+  //   { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+  //   { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+  //   { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+  // ];
 
   return (
     <div>
-      CryptoDetails {coinUuid}
+      CryptoDetails {UUID}
     </div>
   );
 };
